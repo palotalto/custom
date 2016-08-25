39b7a634a0a4f97829fc392376ddf0e73bc094ec
palotalto <bloggingjunioryear@gmail.com>
2016-08-25 12:28:22 -0500

    Update extension.js

--- extension.js
+++ extension.js
@@ -50,7 +50,7 @@
       language: "english",
       chatLink: "https://rawgit.com/basicBot/source/master/lang/en.json",
       scriptLink: "https://rawgit.com/basicBot/source/master/basicBot.js",
-      roomLock: false, // Requires an extension to re-load the script
+      roomLock: true, // Requires an extension to re-load the script
       startupCap: 10, // 1-200
       startupVolume: 0, // 0-100
       startupEmoji: true, // true or false
